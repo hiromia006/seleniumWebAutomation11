d05084b7e5ba2782188920debe9dccd46cb16796
Hiro Mia <hiromia006@gmail.com>
2025-01-18 21:35:01 +0600

    first commit

---
+++ src/test/java/seleniumClass1/LaunchBrowser.java
@@ -0,0 +1,15 @@
+package seleniumClass1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        WebDriver driver=new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://mvnrepository.com/");
+
+        driver.quit();
+    }
+}
