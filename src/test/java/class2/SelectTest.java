9ff270dab1ec7b2644bc35e3acec436ff655677a
Hiro Mia <hiromia006@gmail.com>
2025-01-21 22:34:07 +0600

    Selenium end class

--- src/test/java/class2/SelectTest.java
+++ src/test/java/class2/SelectTest.java
@@ -1,33 +1,32 @@
 package class2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 
-public class NavigateTest {
+public class SelectTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver=new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
-        driver.get("https://www.saucedemo.com/");
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
         System.out.println(driver.getTitle());
         Thread.sleep(3000);
 
-        driver.navigate().to("https://parabank.parasoft.com/");
+        Select sl=new Select(driver.findElement(By.cssSelector("select[name='country']")));
+        sl.selectByIndex(2);
         Thread.sleep(3000);
 
-        driver.navigate().back();
-        Thread.sleep(2000);
+        sl.selectByValue("ANDORRA");
+        Thread.sleep(5000);
 
-        driver.navigate().refresh();
-        Thread.sleep(2000);
-
-        driver.navigate().forward();
-        Thread.sleep(2000);
+        sl.selectByVisibleText("ALBANIA");
+        Thread.sleep(3000);
 
         driver.quit();
-
     }
 }
